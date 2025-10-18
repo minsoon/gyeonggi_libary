77b8588fc078037993bd255792ebeb27fa2f45ab
eunjuKim <eunjukim@eunjuKims-MacBook-Air.local>
2025-10-19 02:07:58 +0900

    실시간 층별 체류 인원 분포 위젯 스타일 업데이트

--- src/features/summary/ui/realtimeFloorDistribution.tsx
+++ src/features/summary/ui/realtimeFloorDistribution.tsx
@@ -16,7 +16,6 @@ const FloorDistribution = () => {
     { floorName: '3F', value: 12, rate: -12, fill: 'url(#grad5)' },
     { floorName: '4F', value: 8, rate: 8, fill: 'url(#grad6)' },
     { floorName: '5F', value: 4, rate: 14, fill: 'url(#grad7)' },
-    { floorName: '기타', value: 5, rate: 0, fill: '#EBF4FF' }, // 빈 공간을 위한 기본값
   ]
 
   const handleClick = () => {
@@ -32,42 +31,27 @@ const FloorDistribution = () => {
         </button>
       </div>
 
+      {/* SVG Gradient 정의 */}
+      <svg width='0' height='0' style={{ position: 'absolute' }}>
+        <DefsGradient />
+      </svg>
+
       <div className={styles.chartTableContainer}>
         <div className={styles.responsiveContainer}>
           <ResponsiveContainer width='100%' height='100%'>
             <PieChart margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
-              <defs>
-                <linearGradient id='grad1' x1='8.12498' y1='8.12503' x2='186.875' y2='195'>
-                  <stop stopColor='#207DD4' />
-                  <stop offset='1' stopColor='#6FB9FF' />
-                </linearGradient>
-                <linearGradient id='grad2' x1='16.25' y1='-1.19847e-05' x2='195' y2='195'>
-                  <stop stopColor='#2AD44C' />
-                  <stop offset='1' stopColor='#89F09D' />
-                </linearGradient>
-                <linearGradient id='grad3' x1='0' y1='0' x2='195' y2='195'>
-                  <stop stopColor='#FFCE00' />
-                  <stop offset='1' stopColor='#FFE888' />
-                </linearGradient>
-                <linearGradient id='grad4' x1='0' y1='0' x2='195' y2='195'>
-                  <stop stopColor='#EE7C26' />
-                  <stop offset='1' stopColor='#FFAE71' />
-                </linearGradient>
-                <linearGradient id='grad5' x1='0' y1='0' x2='195' y2='195'>
-                  <stop stopColor='#D42A2A' />
-                  <stop offset='1' stopColor='#F56767' />
-                </linearGradient>
-                <linearGradient id='grad6' x1='0' y1='0' x2='195' y2='195'>
-                  <stop stopColor='#F42CCD' />
-                  <stop offset='1' stopColor='#FF71E3' />
-                </linearGradient>
-                <linearGradient id='grad7' x1='0' y1='0' x2='195' y2='195'>
-                  <stop stopColor='#883DD4' />
-                  <stop offset='1' stopColor='#C082FF' />
-                </linearGradient>
-              </defs>
+              <DefsGradient />
 
-              <Pie dataKey='value' data={data} innerRadius={55} outerRadius={90} startAngle={90} endAngle={-360}>
+              <Pie
+                dataKey='value'
+                data={data}
+                innerRadius={55}
+                outerRadius={90}
+                startAngle={90}
+                endAngle={-360}
+                cornerRadius={2}
+                paddingAngle={1}
+              >
                 {data.map((entry, index) => (
                   <Cell key={`cell-${index}`} fill={entry.fill} />
                 ))}
@@ -79,32 +63,34 @@ const FloorDistribution = () => {
           <table>
             <thead>
               <tr>
-                <th>
-                  <span>위치</span>
-                </th>
+                <th>위치</th>
                 <th>현재인원</th>
                 <th>변화 비율</th>
               </tr>
             </thead>
             <tbody>
-              {data
-                .filter(item => item.floorName !== '기타')
-                .map(item => {
-                  return (
-                    <tr key={item.floorName}>
-                      <td>
-                        <div className={styles.floorItem}>
-                          <div className={styles.indicator} style={{ background: item.fill }}></div>
-                          <div className={styles.label}>{item.floorName}</div>
-                        </div>
-                      </td>
-                      <td className={styles.value}>{item.value}명</td>
-                      <td className={`${styles.rate} ${item.rate > 0 ? styles.increase : styles.decrease}`}>
-                        {item.rate}%
-                      </td>
-                    </tr>
-                  )
-                })}
+              {data.map(item => {
+                const isIncrease = item.rate > 0
+                const arrowIcon = isIncrease ? 'increase' : 'decrease'
+
+                return (
+                  <tr key={item.floorName}>
+                    <td>
+                      <div className={styles.floorItem}>
+                        <svg width='8' height='8'>
+                          <rect width='8' height='8' rx='1' fill={item.fill} />
+                        </svg>
+                        <div className={styles.label}>{item.floorName}</div>
+                      </div>
+                    </td>
+                    <td className={styles.value}>{item.value}명</td>
+                    <td className={`${styles.rate} ${item.rate > 0 ? styles.increase : styles.decrease}`}>
+                      <img src={`/icon/arrow-${arrowIcon}.svg`} width={16} height={16} alt={arrowIcon} />
+                      <span>{Math.abs(item.rate)}%</span>
+                    </td>
+                  </tr>
+                )
+              })}
             </tbody>
           </table>
         </div>
@@ -114,3 +100,38 @@ const FloorDistribution = () => {
 }
 
 export default FloorDistribution
+
+const DefsGradient = () => {
+  return (
+    <defs>
+      <linearGradient id='grad1' x1='8.12498%' y1='8.12503%' x2='186.875%' y2='195%'>
+        <stop stopColor='#207DD4' />
+        <stop offset='1' stopColor='#6FB9FF' />
+      </linearGradient>
+      <linearGradient id='grad2' x1='16.25%' y1='-1.19847e-05%' x2='195%' y2='195%'>
+        <stop stopColor='#2AD44C' />
+        <stop offset='1' stopColor='#89F09D' />
+      </linearGradient>
+      <linearGradient id='grad3' x1='0%' y1='0%' x2='195%' y2='195%'>
+        <stop stopColor='#FFCE00' />
+        <stop offset='1' stopColor='#FFE888' />
+      </linearGradient>
+      <linearGradient id='grad4' x1='0%' y1='0%' x2='195%' y2='195%'>
+        <stop stopColor='#EE7C26' />
+        <stop offset='1' stopColor='#FFAE71' />
+      </linearGradient>
+      <linearGradient id='grad5' x1='0%' y1='0%' x2='195%' y2='195%'>
+        <stop stopColor='#D42A2A' />
+        <stop offset='1' stopColor='#F56767' />
+      </linearGradient>
+      <linearGradient id='grad6' x1='0%' y1='0%' x2='195%' y2='195%'>
+        <stop stopColor='#F42CCD' />
+        <stop offset='1' stopColor='#FF71E3' />
+      </linearGradient>
+      <linearGradient id='grad7' x1='0%' y1='0%' x2='195%' y2='195%'>
+        <stop stopColor='#883DD4' />
+        <stop offset='1' stopColor='#C082FF' />
+      </linearGradient>
+    </defs>
+  )
+}
